241cb7de2a1018e309ba40cf08b6364f93f6aa66
KyleJ <kjacobson351@gmail.com>
2023-02-06 18:26:47 +0900

    completely refactored js, add light css, make player score update

--- script.js
+++ script.js
@@ -1,143 +1,113 @@
-let computerChoice = 1
-let playerInput = "scissors"
-let match
-let matchCount = 0
-let computerScore = 0
-let playerScore = 0
-let win
-let lose
-let draw
+//get input from player(from buttons)
 
 
+//get computer choice
 
+
+//play round
+
+//evaluate the winner of round dependent on pc / player choices
+
+
+//update UI to show score
+
+
+//play 5 rounds and declare winner
+
+
+//EVENT LISTENERS/////////////////////////////////////
+const rockBtn = document.querySelector('#rockBtn');
+rockBtn.addEventListener('click', playerChoiceRock)
+
+const paperBtn = document.querySelector('#paperBtn');
+paperBtn.addEventListener('click', playerChoicePaper)
+
+const scissorsBtn = document.querySelector('#scissorsBtn');
+scissorsBtn.addEventListener('click', playerChoiceScissors)
+//////////////////////////////////////////////////////
+
+let playerChoice = "";
+let computerChoice = 0;
+let round = 0;
+let playerScore = 0;
+const playerText = document.querySelector("#playerText")
+let computerScore = 0;
+let roundResultMessage = "";
+let finalResultMessage = "";
+
+//these functions change the player choice input based on html button press
+
+function playerChoiceRock(){
+playerChoice = "Rock"
+round++
+getComputerChoice()
+determineRoundWinner(playerChoice,computerChoice)
+playerText.textContent = playerScore;
+declareFinalWinner(playerScore,computerScore)
+};
+
+function playerChoicePaper(){
+playerChoice = "Paper"
+round++
+getComputerChoice()
+determineRoundWinner(playerChoice,computerChoice)
+declareFinalWinner(playerScore,computerScore)
+};
+
+function playerChoiceScissors(){
+    playerChoice = "Scissors"
+    round++
+    getComputerChoice()
+    determineRoundWinner(playerChoice,computerChoice)
+    declareFinalWinner(playerScore,computerScore)
+    };
+
+//Determines computer choice from random number between 0-2
 function getComputerChoice(){
-    computerChoice = Math.random() * 3;
-    computerChoice = Math.floor(computerChoice);
+    computerChoice = Math.floor(Math.random() * 3)
+    
     if (computerChoice === 0) {
-        computerChoice = "Rock" 
+        computerChoice = "Rock";
     } else if (computerChoice === 1) {
-        computerChoice = "Paper"
-    } else {
-        computerChoice = "Scissors"
-    } return computerChoice; 
-    
+        computerChoice = "Paper";
+    } else  if (computerChoice === 2){
+        computerChoice = "Scissors";
+    }else {"getComputerChoice ERROR"};
+};
+
+//determines who the winnder of the round is and awards points accordingly.
+function determineRoundWinner(playerChoice,computerChoice){
+    if (playerChoice == computerChoice) {
+        roundResultMessage = "It's a draw!"
+
+    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        roundResultMessage = "You Win! Rock smashes scissors";
+        playerScore++;
+
+    } else if (playerChoice === "Rock" && computerChoice === "Paper") {
+        roundResultMessage = "You lose! Paper covers rock!";
+        computerScore++;
+
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        roundResultMessage = "You win! Paper covers rock!";
+        playerScore++;
+    } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        roundResultMessage = "You lose! Scissors cuts paper!"
+        computerScore++
+    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        roundResultMessage = "You win! Paper covers rock!";
+        playerScore++;
+    } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+        roundResultMessage = "You lose! Rock smashes scissors!";
+        computerScore++;
+    } else {alert("determineRoundWinner ERROR")}
+}
+// declares to the first player with 5 points
+function declareFinalWinner(playerScore,computerScore) {
+    if (playerScore === 5) {
+        finalResultMessage = "PLAYER WINS"
+    } if (computerScore === 5) {
+        finalResultMessage = "COMPUTER WINS";
     }
-
-
-//This makes PlayerInput case insensitive
-function formatPlayerInput() {
-  playerInput = playerInput.toLocaleLowerCase()
-  firstChar = playerInput.slice(0,1);
-  firstChar = firstChar.toUpperCase()
-  otherChar = playerInput.substring(1)
-  playerChoice = firstChar + otherChar
-  
-}
-
-
-
-
-
-
-
-
-
-
-
-//This takes computer and player choice and determines the winner.
-function rockPaperScissors(){
-    formatPlayerInput();
-match = computerChoice + " " + playerChoice;
-switch (match) {
-    case "Rock Rock":
-        return("It's a draw, TRY AGAIN.");
-    break;
-    case "Rock Paper":
-        return("Paper beats rock. YOU WIN!");
-    break;
-    case "Rock Scissors":
-        return("Rock beats Scissors. YOU LOSE");
-    break;
-    case "Paper Rock":
-        return("Paper beats rock. YOU LOSE!");
-    break;
-    case "Paper Paper":
-        return("It's a draw, TRY AGAIN.");
-    break;
-    case "Paper Scissors":
-        return("Scissors beats paper. YOU WIN!");
-    break;
-    case "Scissors Rock":
-        return("Rock beats scissors. YOU WIN!");
-    break;
-    case "Scissors Paper":
-        return("Scissors beats paper. YOU LOSE!");
-    break;
-    case "Scissors Scissors":
-        return("It's a draw, TRY AGAIN.");
-    break;
-    default: return "error";
-}
-}
-
-
-function playRound(){
-    playerInput = prompt();
-    getComputerChoice();
-    rockPaperScissors();
-    matchCount++;
-    getScore();
-    declareWinner();
-    console.log(computerChoice);
-    console.log(playerChoice);
-    console.log(rockPaperScissors());
-    console.log(declareWinner());
+    };
     
-    
-}
-    
-
- 
-// Checks for a win condition in the rockpaperscissors() return, then distributes points accordingly  
-function getScore(){
-if (rockPaperScissors().includes("YOU WIN!") ){
- playerScore ++
-} else if (rockPaperScissors().includes("YOU LOSE!")){
-    computerScore ++
-} else {}
-}
-
-function resetScore(){
-    playerScore = 0;
-    computerScore =0;
-}
-
-function declareWinner() {
-    if (playerScore > computerScore){
-        return `Computer score: ${computerScore}. Player score: ${playerScore}. YOU WIN`
-    } else if (playerScore < computerScore) {
-        return `Computer score: ${computerScore}. Player score: ${playerScore}. YOU LOSE`
-    }  else {
-        return `Computer score: ${computerScore}. Player score: ${playerScore}. YOU Draw`
-    }
-}
-  
-  
-    
-    
-    
-
-
-//This plays 5 rounds...need to find a way to keep score
-function game() {
-    for (let matchCount = 1; matchCount < 6; matchCount++)
-playRound();
-resetScore()
-}
-
-
-game()
-
-console.log(rockPaperScissors().includes("YOU WIN!"))
-console.log(rockPaperScissors().includes("YOU LOSE!"))
-console.log(rockPaperScissors().includes("TRY AGAIN"))
